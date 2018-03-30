01645f934fbfec33a9b758443b4853cae09db9e9
ajezierska <a.b.jezierska@gmail.com>
2018-03-30 12:40:41 +0200

    beer parameters icons

--- src/components/Form.js
+++ src/components/Form.js
@@ -7,12 +7,33 @@ class Form extends React.Component {
       <form onSubmit={this.props.getBeer}>
         <h2>Find your perfect beer</h2>
 
-      <div className="zippers">
-          <input type="range" name="abv_gt" min="0" max="2" className="zipper"/>
+        <div className="param-container">
+          <p className="param-names">
+            power <br/>
+            biterness <br/>
+            color <br/>
+          </p>
 
-          <input type="range" name="ibu_gt" min="0" max="3" className="zipper"/>
+          <div className="icons">
+            <i className="fas fa-beer fa-xs"></i>
+            <img src="https://png.icons8.com/windows/50/000000/hops.png" alt="hops" width="20px"/>
+            <i className="far fa-circle fa-xs"></i>
+          </div>
 
-          <input type="range" name="ebc_gt" min="0" max="3" className="zipper"/>
+          <div className="zippers">
+              <input type="range" name="abv_gt" min="0" max="2" className="zipper"/>
+
+              <input type="range" name="ibu_gt" min="0" max="3" className="zipper"/>
+
+              <input type="range" name="ebc_gt" min="0" max="3" className="zipper"/>
+          </div>
+
+          <div className="icons">
+            <i className="fas fa-beer fa-lg"></i>
+            <img src="https://png.icons8.com/android/50/000000/hops.png" alt="more hops" width="15px"/>
+            <i className="fas fa-circle fa-xs"></i>
+          </div>
+
         </div>
 
 
